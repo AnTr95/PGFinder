157d4896c09452c65817218542fa7eb7f2b7cd4b
AnTr95 <anton.ronsjo@gmail.com>
2023-04-22 18:42:15 +0200

    added bunch of deprecated files to remove

---
+++ Util/RareDatabase.lua
@@ -0,0 +1,16 @@
+PGF_RareQuestIDs = {
+	["Argus Mount Rares"] = {
+		[49183] = "Blistermaw",
+		[48821] = "Houndmaster Kerrax",
+		[48667] = "Naroua",
+		[48712] = "Sabuul",
+		[48721] = "Skreeg",
+		[48705] = "Venomtail Skyfin",
+		[48812] = "Varga",
+		[48695] = "Wrangler Kravos",
+		[48809] = "Puscilla",
+		[48810] = "Vrax'thul",
+		[48697] = "Kaara the pale",
+	},
+}
+	
